23d87e3c03d0b98e26b80f29ea2302433d44108c
pronane <pronane@yahoo.com>
2017-08-17 00:24:46 +0100

    rest demo add Signed-off-by: pronane <pronane@yahoo.com>

---
+++ app/src/main/java/hostelawaygo/com/rest/Greeting.java
@@ -0,0 +1,20 @@
+package hostelawaygo.com.rest;
+
+/**
+ * Created by marcela on 17/08/2017.
+ */
+
+public class Greeting {
+
+    private String id;
+    private String content;
+
+    public String getId() {
+        return this.id;
+    }
+
+    public String getContent() {
+        return this.content;
+    }
+
+}
