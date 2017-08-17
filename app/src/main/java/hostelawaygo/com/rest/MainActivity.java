d0b673c0a9a2a65a571513583b559b7f8bf9f47d
pronane <pronane@yahoo.com>
2017-08-18 00:33:15 +0100

    updated missing items from xml Signed-off-by: pronane <pronane@yahoo.com>

--- app/src/main/java/hostelawaygo/com/rest/MainActivity.java
+++ app/src/main/java/hostelawaygo/com/rest/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
 
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -67,7 +67,7 @@ public class MainActivity extends ActionBarActivity {
         @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container,
                                  Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+            View rootView = inflater.inflate(R.layout.content_main, container, false);
             return rootView;
         }
     }
